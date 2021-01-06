b748ee3c2efced9ad4bcea896fbf73aa446338ab
Vivian Rogers <vivian.rogers@utexas.edu>
2021-01-05 22:18:01 -0600

    cleaned it up

--- perturb.py
+++ perturb.py
@@ -2,13 +2,14 @@ import numpy as np
 from scipy import ndimage
 from scipy.constants import value as c
 
-s = 200
-w = 1 * c(u'Bohr radius')
+s = 201
+w = 10 * c(u'Bohr radius')
 h = c(u'Planck constant')
 m_e = c(u'electron mass')
-e = c(u'Planck constant')
+e = c(u'atomic unit of charge')
 e_0 = c(u'vacuum electric permittivity')
-eV = c(u'Planck constant')
+eV = c(u'electron volt')
+hbar = h/(2*np.pi)
 
 def indexToLoc(i,j,k):
     loc = np.zeros(3)
@@ -17,14 +18,15 @@ def indexToLoc(i,j,k):
     loc[2] = (k/s - 1/2)*w
     return loc 
 
-print( str(indexToLoc(60,2,18)))
+#print( str(indexToLoc(49,50,51)))
 
 def hamiltonian(wfc):
-    coeff1 = -h**2/(8 * np.pi**2 * m_e)
+    coeff1 = -hbar**2/(2 * m_e)
     coeff2 = -e**2/(4*e_0*np.pi)
     wfc2 = np.zeros((s,s,s))
     #wfc2.fill(0)
-    wfc2 = np.add(coeff1*ndimage.filters.laplace(wfc, mode='constant', cval = 0.0), coeff2*hydrogenPot(wfc))
+    wfc2 = coeff1*ndimage.filters.laplace(wfc, mode='constant', cval = 0.0)
+    #wfc2 = np.add(coeff1*ndimage.filters.laplace(wfc, mode='constant', cval = 0.0), coeff2*hydrogenPot(wfc))
     #i = int(s/4)
     #print("<wfc2|wfc2> = " + str(measure(wfc2)) + ", <wfc1|wfc1> = " + str(measure(wfc))) 
     #E = innerProd(wfc,wfc2)
@@ -51,14 +53,18 @@ def hydrogenPot(wfc):
         for j in range(0,len(grid)):
             for k in range(0,len(grid)):
                 loc = indexToLoc(i,j,k)
-                if(loc[0] != 0 and loc[1] != 0 and loc[2] != 0):
-                    grid[i][j][k] = wfc[i][j][k] * (1/(loc.dot(loc)))
-                else:
-                    grid[i][j][k] = wfc[i][j][k] * 10000000000
+                #if(loc[0] != 0 and loc[1] != 0 and loc[2] != 0):
+                grid[i][j][k] = wfc[i][j][k] * (1/(loc.dot(loc)))
+                #else:
+                #    grid[i][j][k] = wfc[i][j][k] * 10000000000
     return grid 
     
 
 
+
+
+Emin = (1+1+1)*h**2/(2*m_e*w**2) 
+print("Predicted: " + str(Emin))
 wfc = np.random.rand(s,s,s) 
 #print("initial norm: "+ str(measure(wfc)))
 wfc = wfc * 1/measure(wfc) 
